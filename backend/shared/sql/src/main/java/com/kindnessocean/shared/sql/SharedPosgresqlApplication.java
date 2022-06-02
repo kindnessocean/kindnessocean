6d62d7fa698b8bc693f51ce4ba5fed1afcd80e61
Ilya Ilyukou <Ilya_Ilyukou@epam.com>
2022-06-02 12:18:36 +0300

    refactoring

--- backend/shared/sql/src/main/java/com/kindnessocean/shared/sql/SharedPosgresqlApplication.java
+++ backend/shared/sql/src/main/java/com/kindnessocean/shared/sql/SharedPosgresqlApplication.java
@@ -2,8 +2,10 @@ package com.kindnessocean.shared.sql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-sql.properties")
 public class SharedPosgresqlApplication {
     public static void main(String[] args) {
         SpringApplication.run(SharedPosgresqlApplication.class, args);
